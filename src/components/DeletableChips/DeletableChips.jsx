34b2f83e015b47eaf4801ff329d0c8a300f985da
Uros <ostojic.uros@yahoo.com>
2023-06-15 18:57:51 +0200

    login

--- src/components/DeletableChips/DeletableChips.jsx
+++ src/components/DeletableChips/DeletableChips.jsx
@@ -1,14 +1,15 @@
-import * as React from 'react';
-import Chip from '@mui/material/Chip';
-import Stack from '@mui/material/Stack';
+import * as React from "react";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
 
-export default function DeletableChips({chipDelete, text}) {
-
-  
-
+export default function DeletableChips({ chipDelete, text }) {
   return (
     <Stack direction="row" spacing={1}>
-      <Chip label={text} variant="outlined" onDelete={chipDelete} />
+      <Chip
+        label={text}
+        variant="outlined"
+        onDelete={chipDelete}
+      />
     </Stack>
   );
 }
